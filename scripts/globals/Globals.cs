1ab2d0aec7510140e2f5ac564d470bb9c2107d69
p3-j <jako.keskkyla@gmail.com>
2025-03-27 22:30:48 +0200

    init

---
+++ scripts/globals/Globals.cs
@@ -0,0 +1,9 @@
+using Godot;
+using System;
+
+public partial class Globals : Node2D
+{
+
+    [Signal] public delegate void microsoftEventHandler();
+
+}
---
+++ scripts/worldcontroller.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public partial class worldcontroller : Node2D
+{
+
+    Globals glob;
+    public override void _Ready()
+    {
+        base._Ready();
+        glob = GetNode<Globals>("/root/Globals");
+
+        glob.Connect("microsoft", new Callable(this, nameof(TestPrint)));
+        glob.EmitSignal("microsoft");
+       
+    }
+
+    private void TestPrint()
+    {
+        GD.Print("log");
+        GetTree().ChangeSceneToFile("test");
+        GetTree().Paused = true;
+    }
+
+
+}
